3260c6fb1e35c9e64d26154efd886a810f253117
Imran & Jason & Jahed <oss@unrulygroup.com>
2016-02-12 15:49:18 +0000

    List filenames for untracked and modified files.

--- validate-maven-plugin/src/main/java/uk/co/probablyfine/validate/GitUntrackedFilesMojo.java
+++ validate-maven-plugin/src/main/java/uk/co/probablyfine/validate/GitUntrackedFilesMojo.java
@@ -3,6 +3,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.Status;
+import java.util.Arrays;
 
 /**
  * @goal untracked-files
@@ -19,23 +20,23 @@ public class GitUntrackedFilesMojo extends AbstractValidationMojo {
         } catch (Exception e){
             throw new RuntimeException(e);
         }
-        
+
         int untracked = status.getUntracked().size();
         int changed   = status.getChanged().size();
         int modified  = status.getModified().size();
 
         if(untracked > 0) {
-            throw new MojoExecutionException("Untracked files that have not been committed.");
+            throw new MojoExecutionException("Untracked files that have not been committed. Files untracked: "+ Arrays.toString(status.getUntracked().toArray()));
         }
 
         if(changed > 0) {
-            throw new MojoExecutionException("Changed files that have not been committed.");
+            throw new MojoExecutionException("Changed files that have not been committed. Files changed: "+ Arrays.toString(status.getChanged().toArray()));
         }
 
         if(modified > 0) {
-            throw new MojoExecutionException("Modified files that have not been committed.");
+            throw new MojoExecutionException("Modified files that have not been committed. Files modified: "+ Arrays.toString(status.getModified().toArray()));
         }
 
     }
-	
+
 }
